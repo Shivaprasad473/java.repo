a3a5aec1667460bb529672724dac60e762a5c798
Shivaprasad473 <77154337+Shivaprasad473@users.noreply.github.com>
2021-08-23 02:54:45 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ class Sample
 {
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello World");
+System.out.println("Hello World updated");
 }
 
 }
